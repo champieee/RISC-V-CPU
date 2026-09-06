9c29adbf57fedc22f57ef55602829c550f55635e
Ayush Singh <asingh5043@inst.hcpss.org>
2026-09-06 18:03:53 -0400

    Update riscv_cpu.v

--- riscv_cpu.v
+++ riscv_cpu.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 08/13/2026 04:58:47 PM
-// Design Name: 
-// Module Name: riscv_cpu
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module riscv_cpu (
     input wire clk,
