3a3f25c60538d1f5b606d38cdc2c841ad38ad139
Ayush Singh <asingh5043@inst.hcpss.org>
2026-09-06 18:04:07 -0400

    Update basys3_top.v

--- basys3_top.v
+++ basys3_top.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 08/13/2026 07:33:50 PM
-// Design Name: 
-// Module Name: basys3_top
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module basys3_top (
     input wire clk,
